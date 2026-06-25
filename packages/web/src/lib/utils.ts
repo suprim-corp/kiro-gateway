33c7b723848817b277371dad2185b44a0aa29319
Nguyễn Cao Hoài Nam <contacts@sant1ago.dev>
2026-06-26 00:56:39 +0700

    Add shadcn/ui component library with Radix UI and utility setup

---
+++ packages/web/src/lib/utils.ts
@@ -0,0 +1,10 @@
+import { clsx, type ClassValue } from "cnfast"
+import { twMerge } from "cnfast"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function cnfast(...inputs: ClassValue[]) {
+  return clsx(inputs)
+}
